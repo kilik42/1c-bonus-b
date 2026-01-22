53b56413e462fb235a47bc8f3e953ac44f83a67a
kilik42 <marvinevins@yahoo.com>
2026-01-22 00:30:05 -0600

    working on domain

--- 2_locals.tf
+++ 2_locals.tf
@@ -2,7 +2,7 @@
 #local values for existing Chewbacca infrastructure
 
 locals {
-  chewbacca_vpc_id          = "vpc-0b8869d87c2c27e"
+  chewbacca_vpc_id          = "vpc-0b8869d887c22c27e"
   chewbacca_public_subnets  = ["subnet-0b042ad35f85ea27", "subnet-03473bd995f5f8931"]
   chewbacca_ec2_instance_id = "i-0891580f67c9ee103"
   chewbacca_ec2_sg_id       = "sg-0390e8ed3fd3f7f12"
