f8db11d5ed62c3f5edbffeedb4dde530ddd71556
kilik42 <marvinevins@yahoo.com>
2026-02-08 17:07:39 -0600

    working on the tokyo routing

--- lab-3/tokyo/04_route_tables.tf
+++ lab-3/tokyo/04_route_tables.tf
@@ -28,3 +28,55 @@ resource "aws_route_table_association" "tokyo_public_subnet_2_association" {
   route_table_id = aws_route_table.tokyo_public_route_table.id
 }
 
+
+# fix the following code to create a private route table and associate it with the private subnets in tokyo region
+# # create route table for Tokyo VPC
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
+
+#   tags = { Name = "shinjuku-private-rt01" }
+# }
+
+# # create route table association for Tokyo private subnets
+# resource "aws_route_table_association" "private_a" {
+#   subnet_id      = aws_subnet.private_a.id
+#   route_table_id = aws_route_table.private.id
+# }
+
+# resource "aws_route_table_association" "private_b" {
+#   subnet_id      = aws_subnet.private_b.id
+#   route_table_id = aws_route_table.private.id
+# }
+
+# fix this code above
+#create private route table for tokyo region
+resource "aws_route_table" "tokyo_private_route_table" {
+  provider = aws.tokyo
+  vpc_id   = aws_vpc.tokyo_vpc.id
+    tags = {
+        Name = "tokyo_private_route_table"
+    }
+}
+
+# create route to nat gateway for private route table
+resource "aws_route" "tokyo_private_nat_route" {
+  provider = aws.tokyo
+  route_table_id         = aws_route_table.tokyo_private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id         = aws_nat_gateway.tokyo_nat_gw.id
+}
+# associate private subnets with private route table
+resource "aws_route_table_association" "tokyo_private_subnet_1_association" {
+  provider = aws.tokyo
+  subnet_id      = aws_subnet.tokyo_private_subnet_1.id
+  route_table_id = aws_route_table.tokyo_private_route_table.id
+}
+
+
+resource "aws_route_table_association" "tokyo_private_subnet_2_association" {
+  provider = aws.tokyo
+  subnet_id      = aws_subnet.tokyo_private_subnet_2.id
+  route_table_id = aws_route_table.tokyo_private_route_table.id
+}
+
+
--- lab-3/tokyo/06_sg.tf
+++ lab-3/tokyo/06_sg.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "tokyo_public_sg" {
         to_port          = 22
         protocol         = "tcp"
         cidr_blocks      = ["0.0.0.0/0"]
-    }
+    }-
 
     ingress {
         description      = "HTTP from anywhere"
